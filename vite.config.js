fd9fca5daff976b90f1ea71ebbb13457879d4487
Juanryz <yojuanryanto@gmail.com>
2026-04-07 21:27:23 +0700

    Fix ViteManifestNotFoundException: Rebuild assets and track public/build

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
         }),
         react(),
     ],
-    build: {
-        manifest: 'manifest.json',
-    },
+    // build: {
+    //     manifest: 'manifest.json',
+    // },
 });
